a044dd9259cb7bb58516e77f28e44f8a547f10be
sagab77 <64429763+sagab77@users.noreply.github.com>
2021-05-25 14:26:27 +0200

    TP 1

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,13 +1,16 @@
 package dev;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        String asciiArt = FigletFont.convertOneLine("hello");
+        System.out.println(asciiArt);
     }
 }
